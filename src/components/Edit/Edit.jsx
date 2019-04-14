6b06ca1337ace2a3d793cd78c892c4da3717a9c6
Tyler Arnone <tylerarnone@gmail.com>
2019-04-14 18:08:52 -0500

    edit update

--- src/components/Edit/Edit.jsx
+++ src/components/Edit/Edit.jsx
@@ -1,10 +1,92 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 export default class EditRecipe extends Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            recipe_title: '',
+            recipe_directions: '',
+            recipe_ingrediants: '',
+            recipe_time: '',
+            recipe_favorites: false
+        
+
+        }
+    }
+        componentDidMount(){
+            axios.get('http://localhost:3001/recipe/'+this.props.match.params.id)
+            .then(response => {
+                this.setState({
+                    recipe_title      : response.data.recipe_title,
+                    recipe_directions : response.data.recipe_directions,
+                    recipe_ingredients: response.data.recipe_ingredients,
+                    recipe_time       : response.data.recipe_time,
+                    recipe_favorites  : response.data.recipe_favorites
+                })
+                })
+                .catch(function(err) {
+                    console.log(err)
+                })
+            
+        }
+  
+  
     render() {
         return (
             <div>
-            <p> Edit Recipes</p>
+                <h3>update recipe</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Title</label>
+                        <input type="text" className="form-control" 
+                        value={this.state.recipe_title} onChange={this.onChangeRecipeTitle}/>
+
+                    </div>
+                    <div className="form-group">
+                        <label>Time</label>
+                        <input type="text" className="form-control" 
+                        value={this.state.recipe_time} onChange={this.onChangeRecipeTime}/>
+                        
+                    </div>
+                    <div className="form-group">
+                        <label>Ingredients</label>
+                        <input type="text" className="form-control" 
+                        value={this.state.recipe_ingredients} onChange={this.onChangeRecipeIngreients}/>
+                        
+                    </div>
+                    <div className="form-group">
+                        <label>Directions</label>
+                        <input type="text" className="form-control" 
+                        value={this.state.recipe_directions} onChange={this.onChangeRecipeDirections}/>
+                        
+                    </div>
+                    <div className="form-group">
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input"
+                                type="radio"
+                                name="FavOptions"
+                                value="no"
+                                checked={this.state.recipe_favorites==='no'}
+                                onChange={this.onChangeRecipeFavorite} />
+
+                            <label className="form-check-label">no</label>
+                    </div>
+                   
+                    <div className="form-check form-check-inline"></div>
+                        <input className="form-check-input"
+                                type="radio"
+                                name="FavOptions"
+                                value="yes"
+                                checked={this.state.recipe_favorites==='yes'}
+                                onChange={this.onChangeRecipeFavorite} />
+
+                            <label className="form-check-label">yes</label>
+                   </div>
+                  
+                </form>
             </div>
         )
     }
--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,10 +1,61 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Recipe = props => (
+    <tr>
+        <td>{props.recipe.recipe_title}</td>
+        <td>{props.recipe.recipe_time}</td>
+        <td>{props.recipe.recipe_ingredients}</td>
+        <td>{props.recipe.recipe_directions}</td>
+            <tb>
+            <Link to={"/edit/"+ props.recipe._id}>Edit</Link>
+        </tb>
+     </tr>       
+)
 
 export default class RecipeFeed extends Component {
+   
+   constructor(props) {
+       super(props);
+       this.state = {recipes:[]};
+   }
+   componentDidMount() {
+       axios.get('http://localhost:3001/recipes/')
+       .then(response => {
+           this.setState({recipes: response.data});
+       })
+       .catch(function(err) {
+           console.log(err);
+       })
+   }
+   recipeFeed() {
+       return this.state.recipes.map(function(currentRecipe, i){
+           return <Recipe recipe={currentRecipe} key={i}/>
+
+       });
+   }
+   
     render() {
         return (
             <div>
-            <p> Recipe Feed</p>
+                <h2>Recipes</h2>
+                <table className="table table-striped" style={{ marginTop: 20}}>
+                </table>
+                <thread>
+                    <tr>
+                        <th>Title</th>
+                        <th>Time</th>
+                        <th>Ingredients</th>
+                        <th>Directions</th>
+
+
+                    </tr>
+
+                </thread>
+                <tbody>
+                   { this.recipeFeed() } 
+                </tbody>
             </div>
         )
     }
